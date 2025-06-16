6bc43ee1732fe369eef714b48b42e1b96896db79
Andrey Kruglov <kruglov.it@gmail.com>
2025-06-16 19:57:21 +0300

    draft: linters

---
+++ eslint.config.ts
@@ -0,0 +1,135 @@
+import js from '@eslint/js';
+import importPlugin from 'eslint-plugin-import';
+import perfectionist from 'eslint-plugin-perfectionist';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import unusedImports from 'eslint-plugin-unused-imports';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: [
+      '*.config.*',
+      '**/*.d.ts',
+      'dist',
+      'node_modules',
+      'package*.json',
+      'public',
+      'storybook-static',
+    ],
+  },
+  js.configs.recommended,
+  importPlugin.flatConfigs.recommended,
+  importPlugin.flatConfigs.typescript,
+  tseslint.configs.recommendedTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  react.configs.flat.recommended,
+  react.configs.flat['jsx-runtime'],
+  reactHooks.configs['recommended-latest'],
+  {
+    files: ['**/*.{ts,tsx,js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        project: './tsconfig.json',
+        projectService: true,
+        sourceType: 'module',
+        tsconfigRootDir: import.meta.dirname,
+        warnOnUnsupportedTypeScriptVersion: false,
+      },
+      globals: globals.browser,
+    },
+    plugins: {
+      perfectionist,
+      react,
+      'react-refresh': reactRefresh,
+      '@typescript-eslint': tseslint.plugin,
+      'unused-imports': unusedImports,
+    },
+    rules: {
+      '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        {
+          disallowTypeAnnotations: true,
+          fixStyle: 'separate-type-imports',
+          prefer: 'type-imports',
+        },
+      ],
+      '@typescript-eslint/explicit-function-return-type': 'error',
+      '@typescript-eslint/explicit-module-boundary-types': 'error',
+      '@typescript-eslint/no-import-type-side-effects': 'error',
+      '@typescript-eslint/no-unused-expressions': [
+        'error',
+        {
+          allowShortCircuit: true,
+          allowTernary: true,
+        },
+      ],
+      '@typescript-eslint/no-unused-imports': 'off',
+      'unused-imports/no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+          vars: 'all',
+          varsIgnorePattern: '^_',
+        },
+      ],
+      'import/no-unresolved': 'error',
+      'import/no-unused-modules': 'error',
+      'import/order': 'off',
+      'perfectionist/sort-imports': [
+        'error',
+        {
+          type: 'alphabetical',
+          order: 'asc',
+          groups: [
+            'value-builtin',
+            'value-external',
+            ['value-internal', 'value-parent', 'value-sibling'],
+            [
+              'type-import',
+              'type-internal',
+              'type-parent',
+              'type-sibling',
+              'type-index',
+            ],
+            'ts-equals-import',
+            'side-effect-style',
+            'style',
+          ],
+          internalPattern: ['^@components/', '^@services/', '^@pages/', '^@utils/'],
+          newlinesBetween: 'always',
+        },
+      ],
+      'react/jsx-uses-react': 'off',
+      'react/prop-types': 'off',
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'unused-imports/no-unused-imports': 'error',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+      'import/resolver': {
+        node: {
+          extensions: ['.ts', '.tsx', '.js', '.jsx', '.d.ts'],
+        },
+        typescript: {
+          alwaysTryTypes: true,
+          project: './tsconfig.json',
+        },
+      },
+    },
+  },
+  eslintPluginPrettierRecommended
+);
